cf9bddc42f0a6dabc84b20d0959585659fe36373
ShonaOB <shona-obriendev@outlook.com>
2022-08-17 21:20:56 +0000

    Add review detail to views.py and wire up urls and template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.ReviewList.as_view(), name='home'),
+    path('<slug:slug>/', views.ReviewDetail.as_view(), name='review_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     ListView,
     View,
@@ -13,3 +13,20 @@ class ReviewList(ListView):
     template_name = "index.html"
     paginate_by = 6
 
+
+class ReviewDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = TransportReview.objects.order_by('-created_on')
+        review = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if review.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "review_detail.html",
+            {"review": review,
+            "liked": liked,
+            },
+        )
