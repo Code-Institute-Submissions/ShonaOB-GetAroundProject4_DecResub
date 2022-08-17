e3627faecb4ccae2f4410c2bb236abe4ed8a3319
ShonaOB <shona-obriendev@outlook.com>
2022-08-17 20:58:16 +0000

    Wire up urls and html for index

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views.generic import (
+    ListView,
+    View,
+    DetailView,
+    CreateView)
 from .models import TransportReview
 
-class ReviewList(generic.ListView):
+
+class ReviewList(ListView):
     model = TransportReview
-    queryset = TransportReview.objects.order_by('-created_on')
-    template_name = 'index.html'
+    queryset = TransportReview.objects.order_by("-created_on")
+    template_name = "index.html"
     paginate_by = 6
-    
+
