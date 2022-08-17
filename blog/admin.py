60b29d1dd10612d041826f15b844e8d6b9148f36
ShonaOB <shona-obriendev@outlook.com>
2022-08-17 19:06:11 +0000

    Set up admin site and summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import TransportReview
+from .models import Country
+from .models import CityName
+from .models import Sight
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(TransportReview)
+class TransportReviewAdmin(SummernoteModelAdmin):
+    summernote_fields = ('review_body')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('country', 'created_on')
+    list_display = ('title', 'country', 'created_on')
+    search_fields = ('country', 'city', 'sight', 'title', 'rating')
+
+
+admin.site.register(Country)
+
+admin.site.register(CityName)
+
+admin.site.register(Sight)
+
